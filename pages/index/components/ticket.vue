c6f38c529b3a4b06541bd711607e8f006385f22f
biuaxia <biuaxia@qq.com>
2022-02-28 16:10:07 +0800

    feat: 首页表格布局

---
+++ pages/index/components/ticket.vue
@@ -0,0 +1,60 @@
+<template>
+	<view>
+		<!-- ticket -->
+		<u-grid :border="false" col="5" @click="click">
+			<u-grid-item v-for="(baseListItem,baseListIndex) in baseList" :key="baseListIndex">
+				<u-icon :customStyle="{marginTop:20+'rpx'}" :name="baseListItem.src" :size="40" imgMode="widthFix">
+				</u-icon>
+				<text class="grid-text">{{baseListItem.title}}</text>
+			</u-grid-item>
+		</u-grid>
+		<u-toast ref="uToast" />
+	</view>
+</template>
+
+<script>
+	export default {
+		name: 'ticket',
+		data() {
+			return {
+				baseList: [{
+						src: 'cloud://medical-appointments-7b621f545c8.6d65-medical-appointments-7b621f545c8-1309166222/tickets/VIP.svg',
+						title: 'VIP'
+					},
+					{
+						src: 'cloud://medical-appointments-7b621f545c8.6d65-medical-appointments-7b621f545c8-1309166222/tickets/交友.svg',
+						title: '交友'
+					},
+					{
+						src: 'cloud://medical-appointments-7b621f545c8.6d65-medical-appointments-7b621f545c8-1309166222/tickets/火热.svg',
+						title: '火热'
+					},
+					{
+						src: 'cloud://medical-appointments-7b621f545c8.6d65-medical-appointments-7b621f545c8-1309166222/tickets/美颜.svg',
+						title: '美颜'
+					},
+					{
+						src: 'cloud://medical-appointments-7b621f545c8.6d65-medical-appointments-7b621f545c8-1309166222/tickets/音乐.svg',
+						title: '音乐'
+					},
+				]
+			}
+		},
+		methods: {
+			click(name) {
+				this.$refs.uToast.success(`点击了第${name}个`)
+			}
+		}
+	}
+</script>
+
+<style scoped>
+	.grid-text {
+		font-size: 14px;
+		color: #909399;
+		padding: 10rpx 0 20rpx 0rpx;
+		/* #ifndef APP-PLUS */
+		box-sizing: border-box;
+		/* #endif */
+	}
+</style>
--- pages/index/index.vue
+++ pages/index/index.vue
@@ -2,17 +2,23 @@
 	<view>
 		<Search></Search>
 		<Carousel></Carousel>
+		<Ticket></Ticket>
+		<Classify></Classify>
 	</view>
 </template>
 
 <script>
 	import Search from './components/search'
 	import Carousel from './components/carousel'
+	import Ticket from './components/ticket'
+	import Classify from './components/classify'
 
 	export default {
 		components: {
 			Search,
-			Carousel
+			Carousel,
+			Ticket,
+			Classify
 		},
 		data() {
 			return {
