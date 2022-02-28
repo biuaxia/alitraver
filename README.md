a6b8ad08a56955daa95ecace9f1ea8e531e69765
biuaxia <biuaxia@qq.com>
2022-02-28 18:01:01 +0800

    chore: 2022年2月28日18:00:53备份

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # alitraver
 
 uni-app alitraver
+
+## 进度
+
+- 05:21：[批量并发请求数据](https://ke.qq.com/webcourse/450546/100538988#taid=3977040932298738&vid=5285890794669689292)
--- common/cloudfun.js
+++ common/cloudfun.js
@@ -1,5 +1,18 @@
 const db = wx.cloud.database()
 
-var home = function(banner){
-	
+var home = function(a) {
+	return new Promise((resolve, reject) => {
+		const tabReq = db.collection('tabs')
+		tabReq.get()
+			.then((res) => {
+				log(res)
+			})
+			.catch((err) => {
+				log(err)
+			})
+	})
+}
+
+export {
+	home
 }
