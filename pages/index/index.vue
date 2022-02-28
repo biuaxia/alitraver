ab996c2c8a42b8ef3a2f426f39eee2caf199e4a3
biuaxia <biuaxia@qq.com>
2022-02-28 15:43:12 +0800

    feat: 搜索 && 轮播

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,21 +1,23 @@
 <template>
-	<view class="content">
-		<image class="logo" src="/static/logo.png"></image>
-		<view class="text-area">
-			<text class="title">{{title}}</text>
-		</view>
+	<view>
+		<Search></Search>
+		<Carousel></Carousel>
 	</view>
 </template>
 
 <script>
+	import Search from './components/search'
+	import Carousel from './components/carousel'
+
 	export default {
+		components: {
+			Search,
+			Carousel
+		},
 		data() {
 			return {
-				title: 'Hello'
-			}
-		},
-		onLoad() {
 
+			}
 		},
 		methods: {
 
@@ -24,29 +26,5 @@
 </script>
 
 <style>
-	.content {
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-		justify-content: center;
-	}
 
-	.logo {
-		height: 200rpx;
-		width: 200rpx;
-		margin-top: 200rpx;
-		margin-left: auto;
-		margin-right: auto;
-		margin-bottom: 50rpx;
-	}
-
-	.text-area {
-		display: flex;
-		justify-content: center;
-	}
-
-	.title {
-		font-size: 36rpx;
-		color: #8f8f94;
-	}
 </style>
